95a9131d680a5e5b0175bddc6ea24df49f193e05
chris0016 <cper3745@lehmanhs.com>
2022-03-15 22:54:21 -0400

    Initial Commit

---
+++ build/build.cmd
@@ -0,0 +1,7 @@
+copy ..\feature-1-og.html ..\feature-2.html
+copy ..\feature-1-og.html ..\feature-3.html
+copy ..\feature-1-og.html ..\feature-4.html
+copy ..\feature-1-og.html ..\feature-56.html
+copy ..\feature-1-og.html ..\feature-7.html
+copy ..\feature-1-og.html ..\feature-8.html
+copy ..\feature-1-og.html ..\feature-9.html
---
+++ pages/builder.cmd
@@ -0,0 +1,10 @@
+copy ..\project-details.html .\feature-1.html
+copy ..\project-details.html .\feature-2.html
+copy ..\project-details.html .\feature-3.html
+copy ..\project-details.html .\feature-4.html
+copy ..\project-details.html .\feature-56.html
+copy ..\project-details.html .\feature-7.html
+copy ..\project-details.html .\feature-8.html
+copy ..\project-details.html .\feature-9.html
+
+
